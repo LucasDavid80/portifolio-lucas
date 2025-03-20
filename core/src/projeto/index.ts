7bcd6d2e33b81f666e37ced2180dc81f5aa9c6f5
Lucas David <lucasdavidoliveira80@gmail.com>
2025-03-20 16:06:46 +0000

    feat(core): modelo implementado

---
+++ core/src/projeto/index.ts
@@ -0,0 +1,6 @@
+import { Nivel } from './Nivel'
+import { Tipo } from './Tipo'
+import  Projeto  from './projeto'
+
+export { Nivel, Tipo}
+export type { Projeto }
---
+++ core/src/tecnologia/Tecnologia.ts
@@ -0,0 +1,9 @@
+
+export default interface Tecnologia {
+    id: number
+    nome: string
+    descricao: string
+    imagem: string
+    destaque: boolean
+    // projeto: Projeto
+}
